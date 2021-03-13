27760308ffc2307cb9d5a8b72462df35ccea3046
Jonathan Getahun <kgetahun2@gmail.com>
2021-03-12 19:19:01 -0500

    aws

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ app.use(express.static(path.join(__dirname, '/client/build')));
 
 mountRoutes(app);
 app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, '/build/index.html'));
+    res.sendFile(path.join(__dirname, '/build/index.html'));
   });
 
 
