1911b542cf3fe911f724ccba4ea57bf401ab710e
Jonathan Getahun <kgetahun2@gmail.com>
2021-03-12 19:30:01 -0500

    aws

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ app.use(cors());
 app.use(express.json());
 app.use(morgan('dev'));
 app.use(express.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, '/client/build')));
+app.use(express.static(path.join(__dirname, '/build')));
 
 
 mountRoutes(app);
